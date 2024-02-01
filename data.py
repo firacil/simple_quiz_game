cc1d071cb1c37c8f633b25027979ffbc4ceeac73
firacil <firaolmekbib511@gmail.com>
2024-02-01 08:44:20 -0800

    updated

---
+++ data.py
@@ -0,0 +1,53 @@
+quiz_data = [
+        {
+            "question": "Which of those is odd ?"
+            "choices": ["Queue", "Array", "Linked List", "Stack"]
+            "answer": "Array"
+        },
+        {
+            "question": "Which will use the rule of FIFO ?"
+            "choices": ["Hash function", "Queue", "Stack", "Linked List"]
+            "answer": "Queue"
+        },
+        {
+            "question": "Why python is awesome"
+            "choices": ["Readbility and simpilcity", "Extensive libraries", "versatility", "All"]
+            "answer": "All"
+        },
+        {
+            "question": "Which of those is The Community whisperer @ALX_SWE ?"
+            "choices": ["Julien Barieber", "Irene Aragona", "Obed Ehoneh", "Guillaume salva"]
+            "answer": "Irene Aragona"
+        },
+        {
+            "question": "Who is Co founder of alx africa ?"
+            "choices": ["Julien Barieber", "Irene Arogan", "Gullieme", "Firaol Mekebib"]
+            "answer": "Julien Barieber"
+        },
+        {
+            "question": "Who is Founder of Holberton School ?"
+            "choices": ["Julien Barieber", "Irene Arogan", "Fred Swinkler", "Dennis Ritchie"]
+            "answer": "Julien Barieber"
+        },
+        {
+            "question": "Which one of those doesn't exist on the intranet dashboard"
+            "choices": ["QA Reviews", "Projects", "Slack", "Discord"]
+            "answer": "Slack"
+        },
+        {
+            "question": "Who is Curriculum, Quality & Experience Analyst at ALX ? "
+            "choices": ["Julien Barieber", "Irene Aragona", "Amanuel Sisay", "Obed Ehoneah"]
+            "answer": "Obed Ehoneah"
+        },
+        {
+            "question": "Which is not in the curriculum of sprint 1 according to alx_swe ?"
+            "choices": ["python Language", "vi and emacs", "C Language", "Shell Script"]
+            "answer": "python Language"
+        },
+        {
+            "question": "Which cohort was graduated first @ 2024 ?"
+            "choices": ["C-10", "C-11", "C-12", "C-9"]
+            "answer": "C-11"
+        }
+
+# We can add more questions here
